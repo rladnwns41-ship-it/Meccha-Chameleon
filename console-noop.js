52543e1b44367b5dbbf18b769f43282bf6fc21c8
rladnwns41-ship-it <rladnwns41@gmail.com>
2026-07-30 14:11:14 +0900

    Delete console-noop.js

--- console-noop.js
+++
@@ -1,5 +0,0 @@
-(function(){
-  // 프로덕션 렉 감소: console.log 노-옵화 (warn/error는 유지)
-  const noop = () => {};
-  try { console.log = noop; console.debug = noop; console.info = noop; console.trace = noop; } catch(e){}
-})();
